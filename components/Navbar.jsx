f0b8b9107e72beb5bfaf84a7c82cb160b7c5074b
Ange <ishimwenzizaangell@gmail.com>
2025-12-12 21:29:14 +0200

    .

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,7 +3,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Image, StyleSheet } from "react-native";
 
-//Import your screens
+// Import your screens
+import HomeScreen from "../MainPages/Home.jsx";
 import DevotionScreen from "../MainPages/Devotion.jsx";
 import DiscoverScreen from "../MainPages/Discover.jsx";
 import BibleStudyScreen from "../MainPages/Biblestudy.jsx";
@@ -13,70 +14,56 @@ const Tab = createBottomTabNavigator();
 
 export default function Navigation() {
   return (
+    <NavigationContainer>
       <Tab.Navigator
         screenOptions={({ route }) => ({
           headerShown: false,
-
-          // === NAVBAR STYLE ===
           tabBarStyle: {
             height: 70,
             backgroundColor: "#ffffff",
             paddingBottom: 8,
             paddingTop: 8,
           },
-
-          // === ACTIVE / INACTIVE COLORS ===
-          tabBarActiveTintColor: "#582FFB",     // Purple active
-          tabBarInactiveTintColor: "#9E9E9E",   // Grey inactive
-
-          // === ICON RENDERING WITH TINT ===
-          tabBarIcon: ({ focused }) => {
-            let iconSource;
-
-            switch (route.name) {
-            //   case "Home":
-            //     iconSource = require("../assets/images/Home.png");
-            //     break;
-              case "Daily Devotions":
-                iconSource = require("../assets/images/Devotion.png");
-                break;
-              case "Discover":
-                iconSource = require("../assets/images/Discover.png");
-                break;
-              case "Bible Study":
-                iconSource = require("../assets/images/biblestudy.png");
-                break;
-              case "More":
-                iconSource = require("../assets/images/More.png");
-                break;
-            }
-
-            return (
-              <Image
-                source={iconSource}
-                style={[
-                  styles.icon,
-                  {
-                    tintColor: focused ? "#582FFB" : "#9E9E9E",
-                  },
-                ]}
-              />
-            );
-          },
-
-          // === LABEL STYLE ===
+          tabBarActiveTintColor: "#8C4227",
+          tabBarInactiveTintColor: "#9E9E9E",
           tabBarLabelStyle: {
             fontSize: 12,
             fontFamily: "Lato_400Regular",
           },
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            
+            if (route.name === "Home") {
+              iconName = require("../assets/images/Home.png");
+            } else if (route.name === "Daily Devotions") {
+              iconName = require("../assets/images/Devotion.png");
+            } else if (route.name === "Discover") {
+              iconName = require("../assets/images/Discover.png");
+            } else if (route.name === "Bible Study") {
+              iconName = require("../assets/images/biblestudy.png");
+            } else if (route.name === "More") {
+              iconName = require("../assets/images/More.png");
+            }
+
+            return (
+              <Image
+                source={iconName}
+                style={[
+                  styles.icon,
+                  { tintColor: focused ? "#8C4227" : "#9E9E9E" }
+                ]}
+              />
+            );
+          },
         })}
       >
-        {/* <Tab.Screen name="Home" component={HomeScreen} /> */}
-        <Tab.Screen name="Daily Devotions" component={DevotionScreen} /> 
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Daily Devotions" component={DevotionScreen} />
         <Tab.Screen name="Discover" component={DiscoverScreen} />
         <Tab.Screen name="Bible Study" component={BibleStudyScreen} />
         <Tab.Screen name="More" component={MoreScreen} />
       </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
